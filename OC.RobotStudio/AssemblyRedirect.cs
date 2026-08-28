6188b28ab86ac3f0790597a8e0ff686438f12984
Michael Kasseckert <michael.kasseckert@spiratec.com>
2026-08-28 15:17:19 +0200

    feat: adapt to RobotStudio 2025

---
+++ OC.RobotStudio/AssemblyRedirect.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace OC.RobotStudio;
+
+internal static class AssemblyRedirects
+{
+#pragma warning disable CA2255
+    [ModuleInitializer]
+#pragma warning restore CA2255
+    internal static void Init()
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += (_, args) => 
+            new AssemblyName(args.Name).Name == "System.Security" ? 
+                typeof(System.Security.Cryptography.ProtectedData).Assembly : null;
+    }
+}
